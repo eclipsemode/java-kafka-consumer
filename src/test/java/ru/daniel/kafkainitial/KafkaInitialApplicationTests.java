fbc5418c81d1e23da203a732c4c3de7092e91873
eclipsemode <eclipsemod-post@yandex.ru>
2024-07-03 22:23:29 +0300

    initial

---
+++ src/test/java/ru/daniel/kafkainitial/KafkaInitialApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.daniel.kafkainitial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaInitialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
